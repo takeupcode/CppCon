2e30e34d8f733a8e1b22c973a7be35d74b627f83
Wahid Tanner <wahidtanner@gmail.com>
2019-09-19 00:15:49 -0400

    Added test that will detect bug.

--- 2019/UnitTestDemo/Split/StringUtilScenarios.cpp
+++ 2019/UnitTestDemo/Split/StringUtilScenarios.cpp
@@ -21,3 +21,19 @@ SCENARIO( StringUtil, "Operation/Normal", "unit,string", "StringUtil can split s
     expectedStr = "three";
     VERIFY_EQUAL(expectedStr, output[2]);
 }
+
+#if 0
+SCENARIO( StringUtil, "Operation/Edge", "unit,string", "StringUtil can split empty string." )
+{
+    std::string input = "";
+    char delimiter = ' ';
+    
+    auto const & output = splitString(input, delimiter);
+    
+    std::size_t expectedSize = 1;
+    VERIFY_EQUAL(expectedSize, output.size());
+    
+    std::string expectedStr = "";
+    VERIFY_EQUAL(expectedStr, output[0]);
+}
+#endif
